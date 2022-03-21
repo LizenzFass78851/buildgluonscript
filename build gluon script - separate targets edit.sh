8349efa5c13292a796108db2fcd2064386360879
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2022-03-21 16:48:04 +0100

    optimize scripts and add function "dealing with construction errors".

--- build gluon script - separate targets edit.sh
+++ build gluon script - separate targets edit.sh
@@ -3,7 +3,9 @@
 # Build all targets as of v2021.1 gluon, also broken and use all cpu cores (broken can be set in the script)
 # If there were changes to the targets during gluon, this can also be set in the script
 
+
 BROKENS="1"
+
 
 TARGETS="ar71xx-generic
 ar71xx-tiny
@@ -27,15 +29,39 @@ x86-geode
 x86-legacy
 x86-64"
 
+
 git clone --branch v2021.1.1 https://github.com/freifunk-gluon/gluon.git
 cd gluon
 git clone https://gitlab.com/FreifunkChemnitz/site-ffc.git site
 make update
 
+
 for TARG in ${TARGETS}; do
+	echo downloading $TARG
 	make GLUON_TARGET=$TARG BROKEN=$BROKENS -j$(nproc||printf "2") download
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		echo downloading $TARG failed;
+		make GLUON_TARGET=$TARG BROKEN=$BROKENS V=s download
+		RESULT=$?
+		if [ $RESULT -ne 0 ]; then
+			echo downloading $TARG failed again;
+			exit 1;
+		fi
+	fi
 done
 
 for TARG in ${TARGETS}; do
+	echo building $TARG
 	make GLUON_TARGET=$TARG BROKEN=$BROKENS -j$(nproc||printf "2")
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		echo building $TARG failed;
+		make GLUON_TARGET=$TARG BROKEN=$BROKENS V=s
+		RESULT=$?
+		if [ $RESULT -ne 0 ]; then
+			echo building $TARG failed again;
+			exit 1;
+		fi
+	fi
 done
